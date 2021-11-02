23cb8927fef3eb446bda60a054ec4e30e48f950c
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-03 00:22:36 +0400

    Creation of two model Rails app with a restaurant and anonymous reviews.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :create, :new, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
+# get '/restaurants', to: 'restaurants#index'
+# get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+# post '/restaurants', to: 'restaurants#create'
+# get '/restaurants/:id', to: 'restaurants#show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+10.times do
+  p Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+puts "Finished!"
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
